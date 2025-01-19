919ac08a093ece414512627dcca481bf03f58827
Allan Sechrist <sechristallan@gmail.com>
2025-01-19 14:27:33 +0900

    Set up controllers

---
+++ app/controllers/offers_controller.rb
@@ -0,0 +1,18 @@
+class OffersController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  private
+
+  def offer_params()
+  end
+end
