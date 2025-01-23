be59cd3df3f624be9e1ad46e931b7c341b56d3ae
Allan Sechrist <sechristallan@gmail.com>
2025-01-23 21:41:01 +0900

    update game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,12 @@ class Game < ApplicationRecord
 
   validates :igdb_id, presence: true, uniqueness: true
   validates :name, presence: true
+
+  before_save :set_search_name
+
+  private
+
+  def set_search_name
+    self.search_name = name.gsub(/[^a-z0-9]/i, '').downcase
+  end
 end
