4094018b068e3868a18fb3819755af956c6eaf45
Cindy Mae Ngoho <cindymaen17@gmail.com>
2025-01-25 13:35:29 +0900

    made the search for game name

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   get "dashboard/listings/:id/offers", to: "offers#for_listing", as: :dashboard_listing_offers
   get "/dashboard", to: "dashboard#index", as: :dashboard
   post "/listings", to: "listings#create"
+
+  get 'games/search', to: 'games#search'
 end
