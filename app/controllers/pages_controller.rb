2358649e306d52b4037d5c06446afa0f49d69354
Cindy Mae Ngoho <cindymaen17@gmail.com>
2025-01-26 21:36:45 +0900

    added the carousel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @listings = Listing.includes(:game).all.shuffle # Randomize the order of listings for carousel display
     @query = params.dig(:search, :query)
     if @query.present?
       normalized_query = @query.gsub(/[^a-z0-9]/i, '').downcase
