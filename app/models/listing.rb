340969a06a705b87773503fa6e7c6331e54ac993
Alexander Wong <alexwongsydney@gmail.com>
2025-01-28 16:40:44 +0900

    Added geocoder gem and relevant starter files and code for such. Created a locations table which a user will belong to

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,7 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :game
   belongs_to :platform
+  belongs_to :location, through: :user
   has_many :offers, dependent: :destroy
 
   validates :price, presence: true, numericality: { greater_than: 0 }
---
+++ app/models/location.rb
@@ -0,0 +1,6 @@
+class Location < ApplicationRecord
+  has_many :users
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+end
