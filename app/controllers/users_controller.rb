d1e98a5e423eb13e04a9826f93d834b86590d524
Cindy Mae Ngoho <cindymaen17@gmail.com>
2025-01-29 20:16:52 +0900

    able to upload picture on user's information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,30 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:edit, :update]
   def edit
-    @user = current_user
   end
 
   def update
-    @user = current_user
-    if @user.update(user_params)
-      redirect_to dashboard_path, notice: 'Your information has been updated successfully.'
+    if @user.valid_password?(user_params[:current_password])
+      if @user.update(user_params.except(:current_password))
+        bypass_sign_in(@user)
+        redirect_to dashboard_path, notice: 'Your information has been updated successfully.'
+      else
+        render :edit
+      end
     else
+      @user.errors.add(:current_password, "is incorrect")
       render :edit
     end
   end
 
   private
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :profile_picture, :location)
+    params.require(:user).permit(:photo, :location_address, :current_password)
   end
 end
