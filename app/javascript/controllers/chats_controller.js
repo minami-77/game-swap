26f0d3ad49f7a9292c613c3b3fc70573b304596c
Alexander Wong <alexwongsydney@gmail.com>
2025-01-30 20:31:51 +0900

    Messages now refresh every second. This is to get 'live' updates of new messages for every user

--- app/javascript/controllers/chats_controller.js
+++ app/javascript/controllers/chats_controller.js
@@ -5,13 +5,30 @@ export default class extends Controller {
   static targets = ["messageInput", "messageForm", "messagesSection", "messages", "chatsSidebar", "chats"]
 
   connect() {
+    this.interval = setInterval(() => {
+      this.refreshMessages();
+    }, 1000);
   }
 
   async selectChat(event) {
+    const activeChat = document.querySelector(".active");
+    if (activeChat) {
+      activeChat.classList.remove("active");
+    }
+    event.target.classList.add("active");
     const chatId = event.currentTarget.dataset.id;
     const response = await fetch(`/get_messages?id=${chatId}`);
     const data = await response.json();
     this.renderMessagesPartial(data.messages, chatId);
+  }
+
+  async refreshMessages() {
+    const chatId = document.querySelector(".active").dataset.id;
+    if (chatId) {
+      const response = await fetch(`/get_messages?id=${chatId}`);
+      const data = await response.json();
+      this.renderMessagesPartial(data.messages, chatId);
+    }
   }
 
   async newMessage(event, chatId) {
@@ -46,5 +63,9 @@ export default class extends Controller {
     if (lastMessage) {
       lastMessage.scrollIntoView();
     }
+  }
+
+  disconnect() {
+    clearInterval(this.interval);
   }
 }
