700d2d1b02c5ae055a26164fd5633343b6cb6929
Allan Sechrist <sechristallan@gmail.com>
2025-01-30 20:53:07 +0900

    Add limit to user uploaded photos for listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,4 +8,11 @@ class Listing < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, presence: true
   validates :max, presence: true, numericality: { greater_than: 0 }
+  validate :photo_limit
+
+  def photo_limit
+    if photos.count > 4
+      errors.add(:photos, "You can only upload a maximum of 4 photos")
+    end
+  end
 end
